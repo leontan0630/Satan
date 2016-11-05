3669151f3aebd482f086b445eec2acf18ef3089c
leontan0630 <jialuota@buffalo.edu>
2016-11-05 17:48:41 -0400

    first

---
+++ app/src/main/java/biubiubiu/satan/satan1.java
@@ -0,0 +1,19 @@
+package biubiubiu.satan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Leon on 2016/11/5.
+ */
+
+public class satan1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.satan1);
+    }
+}
+
+
---
+++ app/src/main/java/biubiubiu/satan/satan2.java
@@ -0,0 +1,17 @@
+package biubiubiu.satan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Leon on 2016/11/5.
+ */
+
+public class satan2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.satan2);
+    }
+}
